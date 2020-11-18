a559393ebc4383db5d020dc090b21871986937c9
Didnelpsun <2675350965@qq.com>
2020-11-19 00:21:33 +0800

    更新仓库并初始化

--- test.py
+++
@@ -1,7 +0,0 @@
-import requests
-response = requests.get('http://www.baidu.com')
-print(type(response))
-print(response.status_code)
-print(type(response.text))
-print(response.text)
-print(response.cookies)
